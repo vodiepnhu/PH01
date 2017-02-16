dcd2048c7faf393fb5da05a9ea45b60c58ee1beb
Diệp Như <vodiepnhu@gmail.com>
2017-02-16 11:23:58 +0700

    Man Hinh Cho
    Màn hình chờ, chạy mượt

---
+++ PH01/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PH01
+{
+	[Activity(Theme = "@style/Theme.AppCompat.Light.NoActionBar", ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.Orientation, ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+
+		}
+	}
+}
+
